ebd05a1cfd48de5f3128be4bf90a6b843625bbfa
Artem Mylostyvyi <milostivoyartem@gmail.com>
2026-05-12 00:28:29 +0300

    feat: implement King move logic

--- src/models/King.ts
+++ src/models/King.ts
@@ -1,9 +1,10 @@
 import {Piece} from "./Piece.js";
 import {Color} from "./types.js";
 import {DefaultMoveStrategy} from "./strategies/DefaultMoveStrategy.js";
+import {KingMoveStrategy} from "./strategies/KingMoveStrategy.js";
 
 export class King extends Piece{
     constructor(position: {x: number, y: number}, color: Color) {
-        super(position, color, new DefaultMoveStrategy());
+        super(position, color, new KingMoveStrategy());
     }
 }
--- src/models/strategies/KingMoveStrategy.ts
+++ src/models/strategies/KingMoveStrategy.ts
@@ -6,7 +6,7 @@ export class KingMoveStrategy implements MoveStrategy {
         const dx = Math.abs(endX - startX);
         const dy = Math.abs(endY - startY);
 
-        if(dx === 1 && dy === 1){
+        if(dx === 1 && dy === 1 || dx === 1 && dy === 0 || dx === 0 && dy === 1){
             return true;
         }
 
