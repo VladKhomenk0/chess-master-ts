5d56ce95cf960ea6a2288c72b3032637e930aa00
Artem Mylostyvyi <milostivoyartem@gmail.com>
2026-05-18 00:18:23 +0300

    feat: implement is check logic

--- src/core/GameEngine.ts
+++ src/core/GameEngine.ts
@@ -1,5 +1,7 @@
 import { Board } from "../models/Board.js";
-
+import { type Color } from "../models/types.js"
+import {King} from "../models/King.js";
+import {Piece} from "../models/Piece.js";
 export class GameEngine {
     public board: Board;
     public currentPlayer: "white" | "black";
@@ -28,6 +30,21 @@ export class GameEngine {
             return false;
         }
 
+        const targetPiece = this.board.cells[endY]![endX] ?? null;
+
+        this.board.cells[endY]![endX] = piece;
+        this.board.cells[startY]![startX] = null;
+
+        const isSelfCheck = this.isCheck(this.currentPlayer);
+
+        this.board.cells[startY]![startX] = piece;
+        this.board.cells[endY]![endX] = targetPiece ?? null;
+
+        if (isSelfCheck) {
+            console.log("Неможливо зробити хід: ваш король залишиться під шахом!");
+            return false;
+        }
+
         this.executeMove(startX, startY, endX, endY);
         this.switchTurn();
 
@@ -44,5 +61,31 @@ export class GameEngine {
     public switchTurn(): void {
         this.currentPlayer = this.currentPlayer === 'white' ? 'black' : 'white';
         console.log(`Хід передано. Тепер ходять: ${this.currentPlayer}`);
+    }
+
+    public isCheck(color: string): boolean {
+
+        let kingX = 0;
+        let kingY = 0;
+
+        for (let i = 0; i < 8; i++){
+            for (let j = 0; j < 8; j++){
+                if (this.board.cells[j]![i] instanceof King && this.board.cells[j]![i]?.color === color) {
+                    kingX = i;
+                    kingY = j;
+                }
+            }
+        }
+        for (let i = 0; i < 8; i++){
+            for (let j = 0; j < 8; j++){
+                if (this.board.cells[j]![i] instanceof Piece && this.board.cells[j]![i]?.color !== color) {
+                    const piece = this.board.cells[j]![i];
+                    if (piece?.canMove({x: i, y: j}, {x: kingX, y: kingY}, this.board)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
